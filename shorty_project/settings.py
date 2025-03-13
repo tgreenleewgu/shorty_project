f6e6ec8fdf938ce6cd6418c892c02050cea379b4
Travis Greenlee <tgr1196@wgu.edu>
2025-03-13 17:23:37 -0400

    working branch pre-API testing

--- shorty_project/settings.py
+++ shorty_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,6 +83,7 @@ DATABASES = {
     }
 }
 
+MONGODB_URI = config('MONGODB_URI')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
