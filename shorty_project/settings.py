4326f1fdf9a3d80b8895bc29abfd9bc5565ea5d5
Travis Greenlee <tgr1196@wgu.edu>
2025-03-17 19:50:41 -0400

    working version POST and GET functioning with frontend

--- shorty_project/settings.py
+++ shorty_project/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'url_shortener',
     'rest_framework',
+    'corsheaders',
+    
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,6 +130,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:19006",  # Expo web port
+    "http://localhost:19000",  # Expo dev tools
+]
 CORS_ALLOW_METHODS = [
     'DELETE',
     'GET',
